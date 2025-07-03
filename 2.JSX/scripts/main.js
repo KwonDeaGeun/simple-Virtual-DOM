a8483daa79ad7ba53387e4f36ca84db806257158
Daeguen Kwon <dk31415@naver.com>
2025-07-03 16:45:40 +0900

    refactor: for 함수를 전개 연산자로 수정

--- 2.JSX/scripts/main.js
+++ 2.JSX/scripts/main.js
@@ -1,8 +1,4 @@
-function h(type, props) {
-    for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-        children[_key - 2] = arguments[_key];
-    }
-
+function h(type, props, ...children) {
     return {
         type: type,
         props: props,
